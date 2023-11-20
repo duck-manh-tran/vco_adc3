9ff15560afb32cf08231f808b7242f7d9a72f906
duck-manh-tran <ducmanh.m18@gmail.com>
2023-11-21 06:30:33 +0700

    set plotting for any profiles in vco-adc

--- matlab/second_delsig_math.m
+++ matlab/second_delsig_math.m
@@ -1,17 +1,17 @@
 % matlab mathematic model of second order noise shapping
-OSR = 500;
+OSR = 250;
 OBG = 2.5;
-fs = 24*10^6;
-% ntf = synthesizeNTF(3, OSR, 0, OBG, 0);
-% ntf = ()
-% L1 = 1/ntf -1;
-% l = impulse(L1,10);
-% x1 = impulse(c2d(tf([1], [1 0]), 1), 10);
-% x2 = impulse(c2d(tf([1], [1 0 0]), 1), 10);
-% 
-% K = [x1 x2]\l;
-f_sample = 24;
-f_vco = [2.5 7]; f_dco = [2 11];   %configure for 1-bit 1-phase
+f_sample = 24;      % sampling rate in Mega Hezt
+fs = f_sample*10^6;
+bw = fs/(2*OSR);
+
+
+f_vco = [2.5 7]; f_dco = [2 11];   % profile_1: configure for 1-bit 1-phase
+% f_vco = 5*[2.5 7]; f_dco = 5*[2 11];   % profile_1: configure for 1-bit 1-phase
+% f_vco = [2.5 7]; f_dco = [2 11];   % profile_1: configure for 1-bit 1-phase
+
+
+
 f_vco_norm = f_vco/f_sample;
 f_dco_norm = f_dco/f_sample;
 Vin_pp = 0.8;    % input voltage = 0.8 Vpp
@@ -27,7 +27,8 @@ Dc = [0 0 0];
 sys_ct = ss(Ac, Bc, Cc, Dc);
 sys_dt = c2d(sys_ct, 1);
 
-u = 0.32*sin(2*pi*0.2/OSR*(0:1:2^15));
+f_sig = 0.2*fs/OSR
+u = Vin_pp*sin(2*pi*(f_sig/fs)*(0:1:2^15));
 K_offset = ones(1, length(u));
 u_ = [u ; K_offset];
 Ad = sys_dt.A;
@@ -37,20 +38,18 @@ Dd = sys_dt.D;
 
 ABCD = [Ad Bd; Cd Dd];
 [v,xn,xmax,y] = simulateDSM(u_, ABCD, 2, zeros(2,1));
-% plot(v);
+v_norm = v - mean(v);
 
+[P1, f] = periodogram(v_norm, blackmanharris(length(v), 'periodic'), length(v), fs);
 
-[P1, f] = periodogram(v, blackmanharris(length(v), 'periodic'), length(v), fs);
-figure(1);
-
-plot (f, 10*log10(P1)-60);
-xlim([0 4e5]);
-figure(2)
+figure(1)
 plot (v(1000:1100))
-% [P2, f] = periodogram(v-y, hanning(length(v), 'periodic'), length(v), 1);
-% grid on;
-% figure(2);
-% % plot (f, 10*log10(P2));
-% % figure(3);
-% plot(f,10*log10(P1./P2));
-% grid on;
+
+figure(2);
+P1_remove_offset = 10*log10(P1) - max(10*log10(P1));
+plot (f, P1_remove_offset);
+hold on;
+tmp = -300:10:0;
+bw = bw * ones(length(tmp));
+plot (bw, tmp);
+xlim([0 4e4]);
